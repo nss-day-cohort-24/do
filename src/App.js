ceb32177b1fd53b79e1c508a6801ca584583678a
Amber Sharpe <amberjsharpe@gmail.com>
2018-05-16 18:57:10 -0500

    removed CircleButton from App. I was only testing it there

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Signin from './components/signIn/signIn';
-import CircleButtons from './components/mainParts/btn-circles';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Signin />
-        <CircleButtons />
       </div>
     );
   }
