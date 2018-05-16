9516b6b554f3decca9198b0a386e3e6b77d1ac77
Amber Sharpe <amberjsharpe@gmail.com>
2018-05-16 18:39:34 -0500

    added small buttons & svgs

--- src/components/mainParts/btn-circles.js
+++ src/components/mainParts/btn-circles.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import times from '../images/times.svg';
 import heart from '../images/heart.svg';
+import arrow from '../images/undo-alt.svg';
+import marker from '../images/btn-map-marker.svg';
 import './btn-circles.css';
 
 class CircleButtons extends Component {
     render() {
         return (
             <div>
+                <button className="smallCircleButton">
+                    <img src={arrow} alt="yellow circular arrow"/>
+                </button>    
                 <button className="largeCircleButton">
                     <img src={times} alt="red letter X"/>    
                 </button>
                 <button className="largeCircleButton">
                     <img src={heart} alt="green heart"/>
+                </button>
+                <button className="smallCircleButton">
+                    <img src={marker} alt="blue map marker"/>
                 </button>
             </div>
         )
