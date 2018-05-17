f1acf116169bf9f112d7a0d32425f36c1507a975
lapinell <lapinell@gmail.com>
2018-05-17 13:45:28 -0500

    created parkDetails functions

--- src/components/parkdetails.js
+++ src/components/parkdetails.js
@@ -1,1 +1,14 @@
 //includes internal-local components for comments and the add icon
+import React, { Component } from 'react';
+import ParkName from './parkParts/park-name';
+import ParkRating from './parkParts/park-rating';
+import ParkPic from './parkParts/park1.jpg';
+
+export class ParkDetails extends Component {
+    return (
+    <img src={ParkPic} alt="park picture" className="parkpic"/>
+    <h3>{this.props.name}</h3>
+    <h2>{this.props.type}</h2>
+    <ParkRating /> 
+    );
+};
