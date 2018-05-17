81deef7c4fa40c100f2dc912d2a58ecdfe3ef029
Amber Sharpe <amberjsharpe@gmail.com>
2018-05-17 10:19:45 -0500

    added park rating, name components to cardstack

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Signin from './components/signIn/signIn';
+import Cardstack from './components/cardstack';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Signin />
+        {/* <Signin /> */}
+        <Cardstack />
       </div>
     );
   }
--- src/components/cardstack.js
+++ src/components/cardstack.js
@@ -1,10 +1,16 @@
 //the parks/events that you swipe through
 import React, { Component } from 'react';
+import CircleButtons from './mainParts/btn-circles';
+import ParkName from './parkParts/park-name';
+import ParkRating from './parkParts/park-rating';
+
 
 class Cardstack extends Component {
     render(props) {
         return <div>
-            
+             <ParkName />
+             <ParkRating />            
+             <CircleButtons />
         </div>
     }
 }
