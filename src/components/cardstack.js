ddcb78fc8aed784f2b51929de8000bc556e4e7eb
Amber Sharpe <amberjsharpe@gmail.com>
2018-05-17 10:30:03 -0500

    added border to card

--- src/components/cardstack.js
+++ src/components/cardstack.js
@@ -3,14 +3,19 @@ import React, { Component } from 'react';
 import CircleButtons from './mainParts/btn-circles';
 import ParkName from './parkParts/park-name';
 import ParkRating from './parkParts/park-rating';
+import ParkPic from './parkParts/park1.jpg';
+import './cardstack.css';
 
 
 class Cardstack extends Component {
     render(props) {
         return <div>
-             <ParkName />
-             <ParkRating />            
-             <CircleButtons />
+            <div className="parkcard">
+                <img src={ParkPic} alt="park picture" className="parkpic"/>
+                <ParkName />
+                <ParkRating />            
+            </div>
+            <CircleButtons />
         </div>
     }
 }
