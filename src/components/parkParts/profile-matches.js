db59d1c13a2a976ebf6c081c0c4ec4e3874c7529
lapinell <lapinell@gmail.com>
2018-05-22 14:26:50 -0500

    started matches component

---
+++ src/components/parkParts/profile-matches.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ParkRating from './park-rating';
+import ParkAddress from './park-address';
+
+export class FavePark extends React.Component {
+ 
+    render() {
+        const parkid = this.props.name;
+        let userid = this.props.uid;
+        return (
+            <div>
+                <div className="favePark">
+                    <h3 className="pt-2 pr-2 pl-2">{this.toTitleCase(this.props.name)}</h3>
+                    <p><ParkAddress address="Test address"/></p>
+                    <ParkRating /> 
+                </div>
+                <DeleteButton parkName={parkid} userid={userid}/>
+            </div>
+        )
+    }
+}
+
+class DeleteButton extends React.Component {
+    render() {
+        <div>
+            <img src="#" onClick={DeleteFromFB(this.props.parkName, this.props.userid)} />
+        </div>
+    }
+}
